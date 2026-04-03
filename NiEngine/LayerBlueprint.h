97196ba4acd11e4d9dad1eff65321d56b595e276
Wr3tchedTorch <ericmoura511@gmail.com>
2026-04-03 11:09:08 -0300

    finish adding blueprints for tilemap

--- NiEngine/LayerBlueprint.h
+++ NiEngine/LayerBlueprint.h
@@ -15,12 +15,29 @@ struct LayerBlueprint
 
 	std::vector<int> data_ = {};
 	bool visible_ = false;
+
+	float opacity_ = 0;
 };
 
-inline void to_json(json& j, const LayerBlueprint& tb)
+inline void to_json(json& j, const LayerBlueprint& lb)
 {
+	j =
+	{
+		{"x", lb.position_.x},
+		{"y", lb.position_.y},
+		{"name", lb.name_},
+		{"opacity", lb.opacity_},
+		{"visible", lb.visible_},
+		{"data", lb.data_}
+	};
 }
 
-inline void from_json(const json& j, LayerBlueprint& tb)
+inline void from_json(const json& j, LayerBlueprint& lb)
 {
+	j.at("x").get_to(lb.position_.x);
+	j.at("y").get_to(lb.position_.y);
+	j.at("name").get_to(lb.name_);
+	j.at("opacity").get_to(lb.opacity_);
+	j.at("visible").get_to(lb.visible_);
+	j.at("data").get_to(lb.data_);
 }
--- NiEngine/TilesetReference.h
+++ NiEngine/TilesetReference.h
@@ -12,10 +12,17 @@ struct TilesetReference
     std::string file_source_ = "";
 };
 
-inline void to_json(json& j, const TilesetReference& tb)
+inline void to_json(json& j, const TilesetReference& tr)
 {
+	j =
+	{
+		{"firstgid", tr.first_gid_},
+		{"source",   tr.file_source_},
+	};
 }
 
-inline void from_json(const json& j, TilesetReference& tb)
+inline void from_json(const json& j, TilesetReference& tr)
 {
+	j.at("firstgid").get_to(tr.first_gid_);
+	j.at("source").get_to(tr.file_source_);
 }
